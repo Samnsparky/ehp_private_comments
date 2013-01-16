948489bfef537fee05c8c2a1d2c45ed60a22d1ae
A. Samuel Pottinger <Samnsparky@gmail.com>
2013-01-16 08:40:44 -0700

    Message display GUI.

--- ehp_profiles_comments.py
+++ ehp_profiles_comments.py
@@ -58,6 +58,7 @@ class PortfolioOverviewPage(webapp2.RequestHandler):
         template = jinja_environment.get_template('portfolio_overview.html')
         template_vals = get_standard_template_dict()
         owner_name = util.get_full_name_from_email(profile_email)
+        template_vals["cur_section"] = 'overview'
         template_vals["owner_name"] = ' '.join(owner_name)
         template_vals["sections"] = sections
         template_vals["section_statuses"] = section_statuses
@@ -66,25 +67,26 @@ class PortfolioOverviewPage(webapp2.RequestHandler):
 
 
 class PortfolioContentPage(webapp2.RequestHandler):
-    def get(self, target_email, section_name):
+    def get(self, profile_email, section_name):
         cur_user = users.get_current_user()
-        if not account_facade.viewer_has_access(cur_user, target_email):
+        if not account_facade.viewer_has_access(cur_user, profile_email):
             self.redirect(constants.HOME_URL)
 
         new_messages = account_facade.get_new_messages(
-            cur_user, target_email, section_name)
+            cur_user, profile_email, section_name)
         old_messages = account_facade.get_old_messages(
-            cur_user, target_email, section_name)
+            cur_user, profile_email, section_name)
 
-        account_facade.set_viewed(cur_user, target_email, section_name)
+        account_facade.set_viewed(cur_user, profile_email, section_name)
         section_statuses = account_facade.get_updated_sections(
-            cur_user, target_email)
+            cur_user, profile_email)
         sections = constants.PORTFOLIO_SECTIONS
 
         template = jinja_environment.get_template('portfolio_section.html')
         template_vals = get_standard_template_dict()
         owner_name = util.get_full_name_from_email(profile_email)
-        template_vals["owner_name"] = owner_name
+        template_vals["cur_section"] = section_name
+        template_vals["owner_name"] = ' '.join(owner_name)
         template_vals["sections"] = sections
         template_vals["section_statuses"] = section_statuses
         template_vals["new_messages"] = new_messages
